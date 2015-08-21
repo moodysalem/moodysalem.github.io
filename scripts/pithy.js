9a0904ade289ec0c23a241b3d39e50953c8ff744
Moody Salem <moody.salem@gmail.com>
2015-08-20 22:05:24 -0500

    Don't navigate away to external urls

--- scripts/pithy.js
+++ scripts/pithy.js
@@ -1,18 +1,33 @@
-$(function(){
-    $("#gotop").click(function(){
-        jQuery("html,body").animate({
-            scrollTop:0
-        }, 500);
-    });
-    $(window).load(function() {
-		$('#gotop').hide();
-    })
-    
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 300){
-            $('#gotop').fadeIn("fast");
-        } else {
-            $('#gotop').stop().fadeOut("fast");
-        }
-    });
+"use strict";
+$(function () {
+  $("#gotop").click(function () {
+    jQuery("html,body").animate({
+      scrollTop: 0
+    }, 500);
+  });
+  $(window).load(function () {
+    $('#gotop').hide();
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('#gotop').fadeIn("fast");
+    } else {
+      $('#gotop').stop().fadeOut("fast");
+    }
+  });
+
+  $(document).on("click", "[href]", function (e) {
+    var t = $(e.target).closest("[href]").attr("href");
+    if (typeof t !== "string") {
+      return;
+    }
+
+    t = t.toLowerCase();
+    // external URLs should open in new window
+    if (t.indexOf("http://") !== -1 || t.indexOf("https://") !== -1) {
+      e.preventDefault();
+      window.open(t);
+    }
+  })
 });
