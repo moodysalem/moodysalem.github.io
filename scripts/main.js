81d4418e7e815564e7a3fb05ab53726797dea8e6
Moody Salem <moody@fastmodeltechnologies.com>
2017-01-18 14:00:25 -0600

    remove Fastclick and fix post

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,1 @@
-"use strict";
-$(function () {
-  FastClick.attach(document.body);
-
-  $(document).on("click", "[href]", function (e) {
-    var t = $(e.target).closest("[href]").attr("href");
-    if (typeof t !== "string") {
-      return;
-    }
-
-    // external URLs should open in new window
-    if (t.toLowerCase().indexOf("http://") !== -1 || t.toLowerCase().indexOf("https://") !== -1) {
-      e.preventDefault();
-      window.open(t);
-    }
-  })
-});
+main.js
